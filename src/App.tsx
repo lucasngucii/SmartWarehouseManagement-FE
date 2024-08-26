b177f754fe1974ac7f9d233e1cf5d7b472cea729
thienphu <phutot1111@gmail.com>
2024-08-26 13:06:23 +0700

    khoi dong du an

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={"sidebar"}>
+            <div className="sidebar-logo">
+                <img src="../public/logo512.png" alt="Logo"/>
+                <h1>My Website</h1>
+            </div>
+            <nav className="sidebar-nav">
+                <ul>
+                    <li><a href="#dashboard">Dashboard</a></li>
+                    <li><a href="#inventory">Inventory</a></li>
+                    <li><a href="#orders">Orders</a></li>
+                    <li><a href="#settings">Settings</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div className={"container"}>
+
+        </div>
     </div>
   );
 }
